c30e41dd1d57c8f200e85c279e388f966999e5b6
Константин Скакодуб <konstantin.skakodub@brainhub.network>
2025-05-28 13:17:55 -0600

    adding video example

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Gallery from "react-preview-gallery";
 import "react-preview-gallery/style.css";
 
@@ -9,6 +9,8 @@ function App() {
     { src: "../images/blue-watch.jpeg", alt: "blue watch" },
     { src: "../images/white-watch.jpeg", alt: "white watch" },
   ];
+
+  const [isShowVideo, setIsShowVideo] = useState(false);
   return (
     <div
       style={{
@@ -18,7 +20,26 @@ function App() {
     >
       <center>
         <h1>Watchs Gallery</h1>
-        <Gallery imagesArr={imgArr} />
+        <Gallery
+          imagesArr={imgArr}
+          isShowVideo={isShowVideo}
+          videoFile={"../images/apple-watch.mp4"}
+        />
+         <button
+          style={{
+            border: "0px",
+            width: "100px",
+            height: "40px",
+            background: "white",
+            color: "red",
+            cursor: "pointer",
+          }}
+          onClick={() => {
+            setIsShowVideo(!isShowVideo);
+          }}
+        >
+          <strong>{isShowVideo ? "show gallery" : "show video"}</strong>
+        </button>
       </center>
     </div>
   );
